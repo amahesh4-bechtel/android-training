6454b5547e280d51bedeb30d970acf7c7e4a4015
Abhishek Kumar Maheshwari <amahesh4@bechtel.com>
2022-07-08 22:08:19 +0530

    Demo news app - adding screenshots

--- app/src/main/java/com/example/bulletin/network/DataModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bulletin.network
-
-data class DataModel(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
-)
--- app/src/main/java/com/example/bulletin/network/JsonDataModel.kt
+++ app/src/main/java/com/example/bulletin/network/JsonDataModel.kt
@@ -1,10 +1,6 @@
 package com.example.bulletin.network
 
 data class JsonDataModel(
-//    val body: String,
-//    val id: Int,
-//    val title: String,
-//    val userId: Int
     val articles: List<Article>,
     val status: String,
     val totalResults: Int
